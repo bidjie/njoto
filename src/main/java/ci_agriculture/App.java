89ce3a9e2b8beae81293507552bceeef54ec9d46
1199 <1199>
2016-03-14 11:44:10 +0700

    Initial commit

---
+++ src/main/java/ci_agriculture/App.java
@@ -0,0 +1,34 @@
+package ci_agriculture;
+
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+
+import java.io.IOException;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static final String TEST_URL = "http://api.hara.ci-agriculture.com/fieldacquisition/territoryManagement/territoryPlan/organizationUnit/availableDesa/find";
+
+    public static void main ( String[] args ) throws Exception
+    {
+        System.out.println( "Hello World!" );
+        getSomething();
+    }
+
+    public static void getSomething () throws UnirestException {
+        HttpResponse<JsonNode> jsonResponse = Unirest.get(TEST_URL)
+                .header("accept", "application/json")
+                .queryString("organization_unit_id", 7)
+                .queryString("season_id", 5)
+                .queryString("radius", 10)
+                .queryString("show_polygon", false)
+                .asJson();
+        System.out.println(jsonResponse.getBody().toString());
+    }
+}
